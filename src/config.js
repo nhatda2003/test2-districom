acc5d6ba7dcc4f2bf04529f75635b936a5c7d8e9
Duong Anh Nhat <111911492+nhatda2003@users.noreply.github.com>
2023-10-07 21:55:44 +0700

    test2

--- src/config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    awsCreds:{
-        accessKey:'AKIASP6ZYGI4QAYN42UT',
-        secretKey: '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'
-    }
-}
--- src/index.js
+++ src/index.js
@@ -16,24 +16,5 @@ root.render(
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
 
-const express = require('express');
-const app = express();
-const s3Controller = require('./src/s3-controller');
-
-app.get('/', (reg, res) => {
-    res.sendFile(__dirname + '/public/index.html');
-});
-
-
-app.post('/upload-to-s3', s3Controller.s3Upload);
-
-app.get('/all-files', s3Controller.s3Get);
-
-app.get('/get-object-url/:key', s3Controller.getSignedUrl);
-
-const port = process.env.port || '1507';
-app.listen(port, () =>{
-    console.log('App listening on: '+port);
-});
 
 
